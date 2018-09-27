d1c384eb64b1db6e4bc638f59a25df6f4351ec72
nquangvinh <nqvinhcantho@gmail.com>
2018-09-27 16:53:21 +0700

    fix pull request command

--- nqvinh/array/find_missing_number/main.c
+++ nqvinh/array/find_missing_number/main.c
@@ -15,14 +15,15 @@ SOLUTIONS:
 
 void mixArray(unsigned int array[], unsigned int arraySize)
 {
-	int i, tmp;
-	for (i = 0; i < arraySize; i++)
+	int i, tmp, a, b;
+	for (i = 0; i < (arraySize - 1); i++)
 	{
 		// (rand() % (upper - lower + 1)) + lower;
-		// upper = 99, lower = 0;
-		int a = (rand() % (99 - 0 + 1)) + 0;
-		int b = (rand() % (99 - 0 + 1)) + 0;
-
+		// upper = 98, lower = 0;
+		// int a = (rand() % (98 - 0 + 1)) + 0;
+		// int b = (rand() % (98 - 0 + 1)) + 0;
+		a = rand() % (arraySize - 1);
+		b = rand() % (arraySize - 1);
 		// swap 2 array member
 		tmp      = array[a];
 		array[a] = array[b];
@@ -32,29 +33,26 @@ void mixArray(unsigned int array[], unsigned int arraySize)
 
 void initArray(unsigned int array[], unsigned int missingNum, unsigned int arraySize)
 {
+	int flag = 0;
 	if ((missingNum < 1) || (missingNum > 100))
 	{
 		printf("Wrong missing number\n");
 		exit(0);
 	}
 
-	for (int i = 0; i < arraySize; i++)
+	for (int i = 0; i < (arraySize - 1); i++)
 	{
-		if ((i+1) == missingNum)
+		if ((i + 1) == missingNum)
 		{
-			array[i] = 0;
-			continue;
+			flag = 1;
 		}
-		else
-		{
-			array[i] = i + 1;
-		}
+		array[i] = i + 1 + flag;
 	}
 
 	mixArray(array, arraySize);
 }
 
-unsigned int findMissingNumber(unsigned int array[])
+unsigned int findMissingNumber(unsigned int array[], unsigned int arraySize)
 {
 	unsigned int orgSum = 0;
 	unsigned int curSum = 0;
@@ -62,7 +60,7 @@ unsigned int findMissingNumber(unsigned int array[])
 	// sum from 5 to 88: (5+88)*84:2 = 3906
 	orgSum = ((1+100)*100)/2;
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < (arraySize - 1); i++)
 	{
 		curSum += array[i];
 	}
@@ -86,6 +84,6 @@ int main(int argc, char const *argv[])
 	// Init an array with missing number is 2nd arg
 	initArray(array, atoi(argv[1]), arraySize);
 
-	printf("Missing Number is %d\n", findMissingNumber(array));
+	printf("Missing Number is %d\n", findMissingNumber(array, arraySize));
 	return 0;
 }
